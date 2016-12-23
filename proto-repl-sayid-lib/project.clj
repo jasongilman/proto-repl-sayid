c7cfb185ae8f5e13a67eb5957e4ee8b21eb049b5
Jason Gilman <jason@element84.com>
2016-12-22 19:26:02 -0500

    Preparing proto-repl-sayid library for initial push to clojars.

--- proto-repl-sayid-lib/project.clj
+++ proto-repl-sayid-lib/project.clj
@@ -1,6 +1,8 @@
-(defproject proto-repl-sayid "0.1.0-SNAPSHOT"
+(defproject proto-repl-sayid "0.1.0"
   :description "Proto REPL Sayid integration that provides a visual call stack and debugging"
   :url "https://github.com/jasongilman/proto-repl-sayid"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [proto-repl "0.3.1"]
                  [com.billpiel/sayid "0.0.10"]]
